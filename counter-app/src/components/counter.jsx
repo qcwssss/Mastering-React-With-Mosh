9f296f203e391bb985282b7765b6a8e56bbc9c9e
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-30 17:00:03 -0800

    add inline style

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,18 +3,12 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200",
   };
 
-  styles = {
-    fontSize: 40,
-    fontWeight: "bold",
-  };
   render() {
     return (
       <div>
-        {/* <img src={this.state.imageUrl} alt=""></img> */}
-        <span style={this.styles} className="badge bg-primary m-2">
+        <span style={{ fontSize: 50 }} className="badge bg-primary m-2">
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
