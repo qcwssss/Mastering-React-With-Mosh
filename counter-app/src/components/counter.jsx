0efc1d5b54ca4433b2f2c8e80883b20b86beaaad
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-31 11:17:03 -0800

    add increment handler

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,13 +3,33 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 1,
+    tags: ["tag1", "tag2", "tag3"],
+    // tags: [],
   };
+
+  handleIncrement() {
+    console.log("Increment Clicked", this);
+    // this
+    // obj.method() -> return obj
+    // stand alone function -> return window obj/undefined
+  }
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <ul>
+          {this.state.tags.length === 0 && "Please create a new tag!"}
+          {this.state.tags.map((tag) => (
+            <li key={tag}> {tag} </li>
+          ))}
+        </ul>
       </div>
     );
   }
