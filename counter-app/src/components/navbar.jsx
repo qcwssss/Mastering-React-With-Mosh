b4ec551ddcc9e35e3e24df1b895f5b15955d4606
Chen Qiu <qiu.chen1@northeastern.edu>
2022-01-02 10:58:38 -0800

    update navbar in bootstrap@4

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,18 +1,15 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge rounded-pill bg-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+// Stateless Functional Component
+const Navbar = (props) => {
+  return (
+    <nav className="navbar navbar-brand navbar-light bg-light">
+      Navbar{" "}
+      <span className="badge badge-pill badge-secondary">
+        {props.totalCounters}
+      </span>
+    </nav>
+  );
+};
 
 export default Navbar;
