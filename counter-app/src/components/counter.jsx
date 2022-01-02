7e780e4d56171c087737ac190a4c40a832410b87
Chen Qiu <qiu.chen1@northeastern.edu>
2022-01-01 20:14:57 -0800

    add handle delete

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,27 +2,29 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
   };
 
   handleIncrement = (product) => {
     console.log(product);
-    this.setState({ count: this.state.value + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
-    console.log("props", this.props);
-
     return (
       <div>
-        <h4>{this.props.id}</h4>
-        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -11,18 +11,36 @@ class Counters extends Component {
     ],
   };
 
+  handleDelete = (counterId) => {
+    // console.log("Event Handler Called", counterId);
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
-            value={counter.value}
-            selected={true}
-            id={counter.id}
-          >
-            {/* <h4>Counter #{counter.id}</h4> */}
-          </Counter>
+            onDelete={this.handleDelete}
+            counter={counter}
+          ></Counter>
         ))}
       </div>
     );
