bdc40ee076e46993d0497ff4a027f5c5fc8e95f6
Chen Qiu <qiu.chen1@northeastern.edu>
2022-01-11 19:26:10 -0800

    add refs

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -5,8 +5,9 @@ import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navbar";
-import "./App.css";
 import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
+import "./App.css";
 
 class App extends Component {
   state = {
@@ -19,6 +20,7 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/login" component={LoginForm}></Route>
             <Route path="/movies/:id" component={MovieForm}></Route>
             <Route path="/movies" component={Movies}></Route>
             <Route path="/customers" component={Customers}></Route>
