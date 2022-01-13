f1a9d5379da1ba00f6b0b5705d53db8c367d4b76
Chen Qiu <qiu.chen1@northeastern.edu>
2022-01-13 15:32:28 -0800

    update register form

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -8,6 +8,7 @@ import NavBar from "./components/navbar";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
 import "./App.css";
+import RegisterForm from "./components/registerForm";
 
 class App extends Component {
   state = {
@@ -21,6 +22,7 @@ class App extends Component {
         <main className="container">
           <Switch>
             <Route path="/login" component={LoginForm}></Route>
+            <Route path="/register" component={RegisterForm}></Route>
             <Route path="/movies/:id" component={MovieForm}></Route>
             <Route path="/movies" component={Movies}></Route>
             <Route path="/customers" component={Customers}></Route>
