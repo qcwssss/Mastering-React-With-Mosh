4ed1f38265de8eaf4fea150ef4e188ad8690af12
Chen Qiu <qiu.chen1@northeastern.edu>
2022-01-20 17:21:14 -0800

    fix select form

--- vidly/src/components/movieForm.jsx
+++ vidly/src/components/movieForm.jsx
@@ -63,7 +63,7 @@ class MovieForm extends Form {
         <form onSubmit={this.handleSubmit}>
           {this.renderInput("title", "Title")}
           {this.renderSelect("genreId", "Genre", this.state.genres)}
-          {this.renderInput("numberInStock", "Number In Stock")}
+          {this.renderInput("numberInStock", "Number In Stock", "number")}
           {this.renderInput("dailyRentalRate", "Rate")}
           {this.renderButton("Save")}
         </form>
