9f073a8ef80c306b264c876ac84066139ed6d6e5
Chen Qiu <qiu.chen1@northeastern.edu>
2022-01-22 16:08:31 -0800

    update handle unexpected error

--- Section 8- Calling Backend Services/start/http-app/src/App.js
+++ Section 8- Calling Backend Services/start/http-app/src/App.js
@@ -41,18 +41,12 @@ class App extends Component {
     try {
       await axios.delete(apiEndPoints + "/" + post.id);
     } catch (ex) {
-      // Expected (404: not found, 400: bad request) - CLIENT ERRORS
-      // - Display a specific error message
       if (ex.response && ex.response.status === 404) {
         alert("This post has already been deleted.");
       } else {
-        // Unexpected (network down, server down, db down, bug)
-        // - Log them
-        // - Display a generic and friendly error message
         console.log("Logging the error", ex);
         alert("An unexpected error occurred");
       }
-
       this.setState({ posts: originalPosts });
     }
   };
