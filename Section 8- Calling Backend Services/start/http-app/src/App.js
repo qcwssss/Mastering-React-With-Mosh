cf4dd89b02615d7b363d9e7c38582a242b2e7294
Chen Qiu <qiu.chen1@northeastern.edu>
2022-01-22 17:08:24 -0800

    extract config and httpService module

--- Section 8- Calling Backend Services/start/http-app/src/App.js
+++ Section 8- Calling Backend Services/start/http-app/src/App.js
@@ -1,22 +1,7 @@
 import React, { Component } from "react";
-import axios from "axios";
+import http from "./services/httpService";
+import config from "./config.json";
 import "./App.css";
-
-axios.interceptors.response.use(null, (error) => {
-  const expectedError =
-    error.response &&
-    error.response.status >= 400 &&
-    error.response.status < 500;
-
-  if (!expectedError) {
-    console.log("Logging the error", error);
-    alert("An unexpected error occurred");
-  }
-
-  return Promise.reject(error);
-});
-
-const apiEndPoint = "https://jsonplaceholder.typicode.com/posts";
 
 class App extends Component {
   state = {
@@ -24,13 +9,13 @@ class App extends Component {
   };
 
   async componentDidMount() {
-    const { data: posts } = await axios.get(apiEndPoint);
+    const { data: posts } = await http.get(config.apiEndpoint);
     this.setState({ posts });
   }
 
   handleAdd = async () => {
     const obj = { title: "a", body: "b" };
-    const { data: post } = await axios.post(apiEndPoint, obj);
+    const { data: post } = await http.post(config.apiEndpoint, obj);
 
     const posts = [post, ...this.state.posts];
     this.setState({ posts });
@@ -38,7 +23,7 @@ class App extends Component {
 
   handleUpdate = async (post) => {
     post.title = "UPDATED";
-    await axios.put(apiEndPoint + "/" + post.id, post);
+    await http.put(config.apiEndpoint + "/" + post.id, post);
 
     const posts = [...this.state.posts];
     const index = posts.indexOf(post);
@@ -53,14 +38,11 @@ class App extends Component {
     this.setState({ posts });
 
     try {
-      await axios.delete(apiEndPoint + "/33299" + post.id);
+      await http.delete(config.apiEndpoint + post.id);
+      // await axios.delete("s" + apiEndpoint + post.id);
     } catch (ex) {
-      console.log("HANDLE DELETE CATCH BLOCK");
-
       if (ex.response && ex.response.status === 404)
         alert("This post has already been deleted.");
-      else {
-      }
       this.setState({ posts: originalPosts });
     }
   };
