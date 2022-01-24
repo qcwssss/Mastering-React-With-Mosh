cd15f04eded8a75f2a1c86b5918ea4ad8f0ef740
Chen Qiu <qiu.chen1@northeastern.edu>
2022-01-23 16:31:58 -0800

    update logService

--- Section 8- Calling Backend Services/start/http-app/src/index.js
+++ Section 8- Calling Backend Services/start/http-app/src/index.js
@@ -2,17 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import Raven from "raven-js";
+import logger from "./services/logService";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-Raven.config(
-  "https://1b54936834bc4a4a80810dcf0c6fe5fe@o1124833.ingest.sentry.io/6163117",
-  {
-    release: "1-0-0",
-    environment: "development-test",
-  }
-).install();
+logger.init();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
