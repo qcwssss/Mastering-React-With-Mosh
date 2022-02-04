d94d6021972d5897f014a3de56777dcf402131e7
Chen Qiu <qiu.chen1@northeastern.edu>
2022-02-04 11:01:13 -0800

    add header token

--- vidly/src/services/httpService.js
+++ vidly/src/services/httpService.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import logger from "./logService";
 import { toast } from "react-toastify";
+import auth from "./authService";
+
+axios.defaults.headers.common["x-auth-token"] = auth.getJwt();
 
 axios.interceptors.response.use(null, (error) => {
   const expectedError =
