df9a6481ce38174d0312743cbfebfea214f4934d
Chen Qiu <qiu.chen1@northeastern.edu>
2022-02-06 13:06:45 -0800

    commit

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -98,7 +98,7 @@ class Movies extends Component {
   };
 
   render() {
-    const { length: count } = this.state.movies;
+    // const { length: count } = this.state.movies;
     const { pageSize, currentPage, sortColumn, searchQuery } = this.state;
     const { user } = this.props;
 
